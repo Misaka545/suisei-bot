65d78049b732bdd34f9151c9d645b42bcafa8595
Misaka <kienquangv@gmail.com>
2025-10-11 02:25:37 +0700

    Add exp

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ require("dotenv").config();
 const { Client, GatewayIntentBits, Events } = require("discord.js");
 const { loadInteractions } = require("./handlers/interactionHandler");
 const { registerCoreEvents } = require("./handlers/eventHandler");
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; 
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
 });
+
+app.get('/', (req, res) => {
+    res.status(200).send('Bot is alive!');
+});
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Web server listening on port ${port}`);
+});
+
 
 (async () => {
   await registerCoreEvents(client);
